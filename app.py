0ff7e77a58c16adc69ac6a5e25c9d948e72ff813
dashcaj <dustinc7@nycstudents.net>
2024-02-12 14:01:05 -0500

    Factoring

--- app.py
+++ app.py
@@ -1,8 +1,1 @@
-import random
-
-rang = range(1, 10)
-THE_RANDOM_NUMBER = random.choice(rang)
-guess_history = ' '
-guess = 0
-
-hello t t t t t tt t t t t t t t t t t t t t t t
+x = int(input("enter a number to find its factors: "))
