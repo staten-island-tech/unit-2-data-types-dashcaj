23056a0e5f29b54c09acc3cdf2ce0690321ac0f5
dashcaj <dustinc7@nycstudents.net>
2024-02-14 13:30:43 -0500

    duh

--- app.py
+++ app.py
@@ -1,1 +1,5 @@
-x = int(input("enter a number to find its factors: "))
+x = int(input("enter a number between 1 to 100 to find its factors "))
+
+
+if x != range[1,100]:
+    print("The number you entered is not between 1 and 100")
