4ad6fbef106b8817b5b93c0af65a5b69dc6ac5d2
dashcaj <dustinc7@nycstudents.net>
2024-02-14 14:02:00 -0500

    pooppooppeepee

--- app.py
+++
@@ -1,5 +0,0 @@
-x = int(input("enter a number between 1 to 100 to find its factors "))
-
-
-if x != range[1,100]:
-    print("The number you entered is not between 1 and 100")
---
+++ sussygcf!!!!!!!.py
@@ -0,0 +1,16 @@
+
+def samenumberdetector():
+    numberuno = int(input("enter your first number: "))
+    numbertwo = int(input("enter you second number: "))
+    if numberuno/numbertwo == 1:
+        print("It appears that you have entered 2 of the same numbers")
+    else:
+        return numberuno, numbertwo
+
+samenumberdetector()
+
+def overrange():
+    
+    
+
+
