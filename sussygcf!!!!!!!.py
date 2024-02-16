e56a25b42eb8bd74e5c55711fb4f02cb48e50c43
dashcaj <dustinc7@nycstudents.net>
2024-02-16 13:55:04 -0500

    Update sussygcf!!!!!!!.py

--- sussygcf!!!!!!!.py
+++ sussygcf!!!!!!!.py
@@ -1,31 +1,16 @@
+
 numberuno = int(input("enter your first number: "))
 numbertwo = int(input("enter you second number: "))
+for i in (0,numberuno):
+    if (numberuno%i == 0 and numbertwo%i == 0):
+        print('cf')
 
 
-if numberuno/numbertwo == 1:
-        print("It appears that you have entered 2 of the same numbers")
-
-if numberuno > 100 or numbertwo > 100:
-        print("one of your numbers are over the limit of 100")
-
-if numberuno < 1 or numbertwo < 1:
-        print("one of your numbers are below the range")
-
-def factors1():
-    for i in range(1, numberuno + 1):
-        if(numberuno % i == 0):
-            print(i)
 
 
-def factors2():
-    for i in range(1, numbertwo + 1):
-        if(numbertwo % i == 0):
-            print(i)
-        
-commonfactors = []
-for i in factors1():
-    if i == i in factors2():
-        commonfactors.append(i)
+
+
+
 
 
 
