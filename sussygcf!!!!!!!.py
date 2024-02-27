b18fdec918d87f0b1c598e1a443f35fbac522f88
dashcaj <dustinc7@nycstudents.net>
2024-02-27 13:48:55 -0500

    Now its done

--- sussygcf!!!!!!!.py
+++ sussygcf!!!!!!!.py
@@ -9,30 +9,22 @@ if numberuno < 1 or numbertwo < 1:
         print("one of your numbers are below the range")
 
 commonfactors1 = []
-commonfactors2 = []
+
 
 THECOMMONFACTORS = []
 
 
-def factors1():
+def factors():
     for i in range(1, numberuno + 1):
-        if(numberuno % i == 0):
+        if(numberuno % i == 0 and numbertwo % i == 0):
             commonfactors1.append(i)
-
-def factors2():
-    for i in range(1, numbertwo + 1):
-        if(numbertwo % i == 0):
-            commonfactors2.append(i)
-factors1()
-factors2()
-
-for i in commonfactors1 and commonfactors2:
-    if i == i in commonfactors1 and commonfactors2:
+factors()
+for i in commonfactors1:
+    if i == i in commonfactors1:
         THECOMMONFACTORS.append(i)
-    if i != i in commonfactors1 and commonfactors2:
+    if i != i in commonfactors1:
         print("no gcf")
 
-    
 
 sus = max(THECOMMONFACTORS)
 print(f"Your greatest common factors is: {sus}")
